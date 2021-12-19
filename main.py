b1ac9c86ed0a9afce393c13d2884d2f029c9c19a
İremnur Zengin <62605439+Iremnurzengin@users.noreply.github.com>
2021-12-19 00:25:53 +0000

    26.-42

--- main.py
+++ main.py
@@ -23,3 +23,27 @@ omega_max= 2 * np.pi * 0.5e6 #hz
 alpha_max= 2 * np.pi * 0.5e6 #hz
 nu = 2 * np.pi * 0.5 * 1e6 # hz
 
+with qctrl.create_graph[] as graph 
+
+#create optimizable pwc controls for omega and sigma
+#omega(t) signal 
+omega = qctrl.operations.pwc_signal(values=qctrl.operations.optimization_variable(count=segment_count, lower_bound=0, upper_bound=omega_max),duration=duration, name='Ω(t)')
+
+#alpha(t) signal
+omega = qctrl.operations.pwc_signal(values=qctrl.operations.optimization_variable(count=segment_count, lower_bound=-alpha_max, upper_bound=alpha_max),duration=duration, name='α(t)')
+
+#Create PWC operators representing each term.
+#omega_term = omega * sigma_x
+omega_term = qctrl.operations.pwc_operator(signal=omega, operator= sigma_x)
+
+# alpha_term = alpha * sigma_z / 2 
+alpha_term = qctrl.operations.pwc_operator(signal=alpha, operator=sigma_z)
+
+#detuning = nu* sigma_z / 2
+
+
+
+
+
+
+
